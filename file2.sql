cce06ecdc8dc5c3d76a27032503d5028c0b0375c
rioter1 <prateek.gsharma@gmail.com>
2023-05-15 10:17:19 +0530

    table alteration

--- file2.sql
+++ file2.sql
@@ -10,6 +10,7 @@ SQL Alter table
 create database customer;
 show databases;
 use customer;
+# auto_incrememtn if called an sql constraint
 create table customer_info(
 id integer auto_increment,
 first_name varchar(25),
@@ -18,9 +19,8 @@ salary integer,
 primary key(id)
 );
 
-select * from customer_info;
 
-insert into customer_info(first_name, last_name_salary)
+insert into customer_info(first_name, last_name, salary)
 values
 ('x', 'y', 50000),
 ('a', 'b', 60000),
@@ -28,7 +28,37 @@ values
 ('c', 'd', 40000),
 ('e', 'f', NULL);
 
+select * from customer_info where salary is null;
+# sql update statemenet to replace null values
+# writing update query
+update customer_info set salary=50000 where id=5;
 select * from customer_info;
 
-drop database customer;
+#sql delete statement
+delete from customer_info where id=5;
+
+# sql alter table
+
+ # add column in existing table
+ 
+alter table customer_info add email varchar(25);
+
+select * from customer_info;
+
+alter table customer_info add dob date;
+select * from customer_info;
+
+# alter table modify coulmn and column type
+
+alter table customer_info modify dob year;
+desc customer_info;
+select * from customer_info;
+
+# use alter table to drop column
+
+alter table customer_info drop column email;
+
+#drop database customer;
+
+
 
